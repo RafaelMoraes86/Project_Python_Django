f276047226dd5c49703766ef4d22d1d675161b46
Rafael Moraes <rafael_hot86@hotmail.com>
2023-06-23 16:00:21 -0300

    Adicionado html
    Adicionado arquivo HTML para visualização no Browser.

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from core.models import Event
+from core.models import Evento
+
+
+
+class EventoAdmin(admin.ModelAdmin) :
+    list_display = ('titulo', 'data_evento', 'data_descricao')
+    list_filter = ('usuario', 'data_evento',)
 
 # Register your models here.
-admin.site.register(Event)
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,11 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Event (models.Model) :
-    title = models.CharField( max_length=100)
-    descrition = models.TextField( blank= True, null= True)
-    date_event = models.DateTimeField()
-    date_criation = models.DateTimeField(auto_now=True)
+class Evento (models.Model) :
+    titulo = models.CharField( max_length=100)
+    descricao = models.TextField( blank= True, null= True)
+    data_evento = models.DateTimeField( verbose_name='Data do Evento')
+    data_descricao= models.DateTimeField( auto_now= True, verbose_name='Data de Criacao')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    
+    def data_event (self):
+        return self.data_evento.strftime('%d/%m/%Y')
 
+def str (self) :
+    return self.titulo
+
+    
    
